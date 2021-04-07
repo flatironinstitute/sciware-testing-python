21478ddc10e27cb753ae8c6e5c5856376cf50848
Adam Lamson <lamsoa729@gmail.com>
2021-04-07 16:00:54 -0400

    Added commented out cpp code to tranform to python and some doctests that may or may not work.

--- sciware_testing_python/sciware_testing_python.py
+++ sciware_testing_python/sciware_testing_python.py
@@ -18,6 +18,12 @@ def sum_numbers(number_list):
     Notes
     -----
     This is NOT good Python, just an example function for tests.
+
+    Examples
+    --------
+    >>> number_list = [2,3,4]
+    >>> sum_numbers(number_list)
+    9
     """
 
     sum_val = 0
@@ -43,6 +49,13 @@ def max_number(number_list):
     Notes
     -----
     Also not good Python.
+
+
+    Examples
+    --------
+    >>> number_list = [2,3,4]
+    >>> max_number(number_list)
+    4
     """
 
     cur_max = number_list[0]
@@ -51,3 +64,33 @@ def max_number(number_list):
             cur_max = n
 
     return cur_max
+
+# std::vector<double> add_vectors(std::vector<double> &v1, std::vector<double> &v2)
+# {
+#     // Check inputs
+#     if (v1.size() != v2.size())
+#     {
+#         throw std::invalid_argument("There's a mismatch in vector size" + std::to_string(v1.size()) + "!=" + std::to_string(v2.size()));
+#     }
+
+#     //
+#     // Solution 1
+#     //
+
+#     // Add the vectors
+#     std::vector<double> new_vector(v1.size());
+
+#     for (size_t i = 0; i < v1.size(); i++)
+#     {
+#         new_vector[i] = v1[i] + v2[i];
+#     }
+
+#     //
+#     // Solution 2
+#     //
+
+#     // std::vector<double> new_vector = v1;
+#     // std::transform(new_vector.begin(), new_vector.end(), v2.begin(), new_vector.begin(), std::plus<double>());
+
+#     return new_vector;
+# }
--- tests/test_sciware_testing_python.py
+++ tests/test_sciware_testing_python.py
@@ -32,6 +32,88 @@ def test_max_number(generate_numbers):
     assert our_result == max(generate_numbers)
 
 
+def test_adding_empty_lists():
+    """TODO: Docstring for test_adding_empty_vectors.
+    @return: TODO
+
+    """
+    pass
+
+# TEST_CASE("Test Adding Empty Vectors")
+# {
+#     std::vector<double> x(0);
+#     auto y = add_vectors(x, x);
+
+#     // Check that we got the right answer
+#     CHECK(y.size() == 0);
+# }
+
+
+def test_adding_singleton_lists():
+    # TEST_CASE("Test Adding Singleton Vectors")
+    # {
+    #     std::vector<double> x = {2};
+    #     std::vector<double> y = {3};
+    #     auto z = add_vectors(x, y);
+
+    #     // Check that we got the right answer
+    #     CHECK(z.size() == 1);
+    #     CHECK(z[0] == 5);
+
+    #     // Check that we didn't modify the original vectors
+    #     CHECK(x[0] == 2);
+    #     CHECK(y[0] == 3);
+    # }
+    pass
+
+
+def test_adding_lists():
+    # TEST_CASE("Test Adding Vectors")
+    # {
+    #     // Set up RNG
+    #     std::random_device rd;
+    #     std::default_random_engine eng(rd());
+    #     std::uniform_real_distribution<double> distr(10, 100);
+
+    #     // Set up vector
+    #     const int N = 100;
+    #     std::vector<double> v1(N);
+    # std::generate(v1.begin(), v1.end(), [&distr, &eng]() { return
+    # distr(eng); });
+
+    #     // Check than if we add it to the negative version of itself, things cancel
+    #     std::vector<double> v2 = v1;
+    #     std::for_each(v2.begin(), v2.end(), [](double &d) { d *= -1; });
+    #     auto v3 = add_vectors(v1, v2);
+    #     for (auto v3_i : v3)
+    #     {
+    #         CHECK(v3_i == 0);
+    #     }
+    # }
+    pass
+
+
+def test_adding_lists_with_bad_args():
+    """TODO: Docstring for test_adding_lists_with_bad_args.
+    @return: TODO
+
+    """
+# TEST_CASE("Test Adding Vectors with Bad Args")
+# {
+#     // Setup
+#     const int N = 10, M = 100;
+#     std::vector<double> v1(N);
+#     std::vector<double> v2(M);
+
+#     // Generally check that it throws an exception
+#     REQUIRE_THROWS(add_vectors(v1, v2));
+
+#     // Check that it throws a specific type of exception
+#     REQUIRE_THROWS_AS(add_vectors(v1, v2), std::invalid_argument);
+# }
+    pass
+
+
 def du_dt_gen_2ord(r_ij, u_i, u_j, rsqr, a_ij, a_ji, b,
                    mu10, mu11, mu20,
                    ks, ho, grot):
