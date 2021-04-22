1b29e660c4fbc88054bd0540697abd6764674390
Adam Lamson <lamsoa729@gmail.com>
2021-04-22 18:41:20 -0400

    Finished adding testing functions

--- tests/test_sciware_testing_python.py
+++ tests/test_sciware_testing_python.py
@@ -24,16 +24,21 @@ def test_adding_vectors():
 
 
 def test_add_zero_length_vectors():
-    """ """
+    """ Test function for adding empty vectors"""
     assert stp.add_vectors([], []) == []
 
 
-############################################################
+#######################################################################
+#                        Advance pytest topics                        #
+#######################################################################
 # Pytest useful decorators
 # - fixture
-# - mark.skip
-# - mark.parametrize
 # - mark.xfail
+# - mark.parametrize
+# - mark.skip
+
+# Possible other discussion
+# - pytest.approx(val, rel=1e-5)
 
 
 @pytest.fixture
@@ -46,10 +51,11 @@ def generate_numbers():
     return random.sample(range(100), 10)
 
 
+# shortcuts for pytest decorators
 skip = pytest.mark.skip
 parametrize = pytest.mark.parametrize
 xfail = pytest.mark.xfail
-###########################################################
+#######################################################################
 
 
 def test_random_sum_numbers(generate_numbers):
@@ -67,42 +73,34 @@ def test_random_sum_numbers_add_1(generate_numbers):
     assert our_result == sum(generate_numbers) + 1
 
 
+@xfail
+def test_sum_numbers_with_bad_args():
+    """ Test sum numbers when not given good input
+    """
+    bad_number_list = ['these', 'R', False, "#s"]
+    result = stp.sum_numbers(bad_number_list)
+
+
+@pytest.mark.parametrize("number_list,  expect_val", [
+    ([1, 2, 3], 6),
+    ([-2, 0, 1], -1),
+    ([3.5, 7.25, -.75], 10),
+])
+def test_parametrize_sum_numbers(number_list, expect_val):
+    assert stp.sum_numbers(number_list) == expect_val
+
+
 @xfail(strict=True)
 def test_add_different_length_vectors():
-    """TODO: Docstring for test_add_zero_length_vectors.
-    @return: TODO
-
     """
-    assert False
+    """
+    v1 = [2, 3, 4]
+    v2 = [4, 9, 16, 25]
+    result = stp.add_vectors(v1, v2)  # doctest: +IGNORE_EXCEPTION_DETAIL
 
 
 @skip
 def test_add_skip_test():
-    """TODO: Docstring for test_add_zero_length_vectors.
-    @return: TODO
-
+    """ This test would fail if not for the decorator
     """
     assert False
-
-
-@xfail
-def test_adding_vectores_with_bad_args():
-    """TODO: Docstring for test_adding_lists_with_bad_args.
-    @return: TODO
-
-    """
-    assert False
-# TEST_CASE("Test Adding Vectors with Bad Args")
-# {
-#     // Setup
-#     const int N = 10, M = 100;
-#     std::vector<double> v1(N);
-#     std::vector<double> v2(M);
-
-#     // Generally check that it throws an exception
-#     REQUIRE_THROWS(add_vectors(v1, v2));
-
-#     // Check that it throws a specific type of exception
-#     REQUIRE_THROWS_AS(add_vectors(v1, v2), std::invalid_argument);
-# }
-    pass
