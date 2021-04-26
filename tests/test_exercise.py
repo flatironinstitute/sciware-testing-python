b1804a53d53b758defca22ffddee209c3bf7a130
Dylan Simon <dylan@dylex.net>
2021-04-26 10:11:36 -0400

    add a bit more template for exercises

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -3,8 +3,12 @@ import sciware_testing_python
 
 def test_sum1():
     # basic sanity test: do we get the right answer for a simple case?
-    assert True
+    assert True #sciware_testing_python.sum_numbers([1,2,3]) == ?
 
 def test_sum2():
     # what's the sum of an empty list
+    pass
+
+#@pytest.mark.xfail(strict=True)
+def test_sum_strings():
     pass
